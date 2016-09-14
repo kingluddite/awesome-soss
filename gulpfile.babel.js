6c63aba20ede7189ae17d032061d61eda3e0617d
Philip Howley <howley.phil@gmail.com>
2016-09-13 18:12:48 -0700

    fix spacing in code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -125,34 +125,36 @@ gulp.task( 'html', () => {
  */
 
 gulp.task( 'sass', () => {
-	return gulp.src( currentEnv.src.sass + 'style.scss' )
-		.pipe( $.plumber() )
-		// expanded
-		.pipe( $.sourcemaps.init() )
-		.pipe( $.sass().on( 'error', $.sass.logError ) )
-		.pipe( $.autoprefixer( {
-			browsers: [ 'last 4 version' ]
-		} ) )
-		.pipe( gulp.dest( currentEnv.dist.css ) )
-		// compressed
-		// minify the concatenated CSS
-		.pipe( $.cssnano() )
-		.pipe( $.rename( {
-			suffix: '.min'
-		} ) )
-		.pipe( $.header( banner, {
-			thePackage: thePackage
-		} ) )
-		.pipe( $.sourcemaps.write() )
-		.pipe( gulp.dest( currentEnv.dist.css ) )
-		.pipe( browserSync.reload( {
-			stream: true
-		} ) );
+  return gulp.src( currentEnv.src.sass + 'style.scss' )
+    .pipe( $.plumber() )
+    // expanded
+    // sourcemaps help us with debugging
+    .pipe( $.sourcemaps.init() )
+    .pipe( $.sass().on( 'error', $.sass.logError ) )
+    // add all the browser prefixes
+    .pipe( $.autoprefixer( {
+      browsers: [ 'last 4 version' ]
+    } ) )
+    .pipe( gulp.dest( currentEnv.dist.css ) )
+    // compressed
+    // minify the concatenated CSS
+    .pipe( $.cssnano() )
+    .pipe( $.rename( {
+      suffix: '.min'
+    } ) )
+    .pipe( $.header( banner, {
+      thePackage: thePackage
+    } ) )
+    .pipe( $.sourcemaps.write() )
+    .pipe( gulp.dest( currentEnv.dist.css ) )
+    .pipe( browserSync.reload( {
+      stream: true
+    } ) );
 } );
 
 gulp.task( 'html', function () {
-	return gulp.src( currentEnv.src.html + '*.html' )
-		.pipe( gulp.dest( currentEnv.dist.html ) );
+  return gulp.src( currentEnv.src.html + '*.html' )
+    .pipe( gulp.dest( currentEnv.dist.html ) );
 } );
 
 /**
@@ -161,16 +163,26 @@ gulp.task( 'html', function () {
  *
  */
 
-
 gulp.task( 'images', function () {
-	// grab only the stuff in images with these extensions {png,jpg,gif,svg,ico}
-	return gulp.src( currentEnv.src.images + '**/**/*.{png,jpg,gif,svg,ico}' )
-		.pipe( $.newer( currentEnv.dist.images ) )
-		.pipe( $.imagemin( {
-			progressive: true
-		} ) )
-		.pipe( gulp.dest( currentEnv.dist.images ) );
+  // grab only the stuff in images with these extensions {png,jpg,gif,svg,ico}
+  return gulp.src( currentEnv.src.images + '**/**/*.{png,jpg,gif,svg,ico}' )
+    .pipe( $.newer( currentEnv.dist.images ) )
+    .pipe( $.imagemin( {
+      progressive: true
+    } ) )
+    .pipe( gulp.dest( currentEnv.dist.images ) );
 } );
+
+/**
+ *
+ * Fonts
+ *
+ */
+gulp.task( 'fonts', function () {
+  return gulp.src( 'src/fonts/**/*' )
+    .pipe( gulp.dest( 'dist/fonts' ) )
+} )
+
 
 /**
  *
@@ -179,27 +191,27 @@ gulp.task( 'images', function () {
  */
 
 gulp.task( 'js', () => {
-	gulp.src( currentEnv.src.js + 'scripts.js' )
-		.pipe( $.sourcemaps.init() )
-		.pipe( $.jshint( '.jshintrc' ) )
-		.pipe( $.jshint.reporter( 'default' ) )
-		.pipe( $.header( banner, {
-			thePackage: thePackage
-		} ) )
-		.pipe( gulp.dest( currentEnv.dist.js ) )
-		.pipe( $.uglify() )
-		.pipe( $.header( banner, {
-			thePackage: thePackage
-		} ) )
-		.pipe( $.rename( {
-			suffix: '.min'
-		} ) )
-		.pipe( $.sourcemaps.write() )
-		.pipe( gulp.dest( currentEnv.dist.js ) )
-		.pipe( reload( {
-			stream: true,
-			once: true
-		} ) );
+  gulp.src( currentEnv.src.js + 'scripts.js' )
+    .pipe( $.sourcemaps.init() )
+    .pipe( $.jshint( '.jshintrc' ) )
+    .pipe( $.jshint.reporter( 'default' ) )
+    .pipe( $.header( banner, {
+      thePackage: thePackage
+    } ) )
+    .pipe( gulp.dest( currentEnv.dist.js ) )
+    .pipe( $.uglify() )
+    .pipe( $.header( banner, {
+      thePackage: thePackage
+    } ) )
+    .pipe( $.rename( {
+      suffix: '.min'
+    } ) )
+    .pipe( $.sourcemaps.write() )
+    .pipe( gulp.dest( currentEnv.dist.js ) )
+    .pipe( reload( {
+      stream: true,
+      once: true
+    } ) );
 } );
 
 /**
@@ -210,16 +222,16 @@ gulp.task( 'js', () => {
 
 // enable Gulp to spin up a server
 gulp.task( 'browser-sync', () => {
-	browserSync.init( null, {
-		// let BrowserSync know where the root of the should be (`dist`)
-		server: {
-			// our root distribution (production) folder is `dist`
-			baseDir: 'dist'
-		}
-	} );
+  browserSync.init( null, {
+    // let BrowserSync know where the root of the should be (`dist`)
+    server: {
+      // our root distribution (production) folder is `dist`
+      baseDir: 'dist'
+    }
+  } );
 } );
 gulp.task( 'bs-reload', () => {
-	reload();
+  reload();
 } );
 
 /**
@@ -227,8 +239,40 @@ gulp.task( 'bs-reload', () => {
  * Cleanup - Blow up everything inside `dist/assets` when you run gulp
  *
  */
-
+// Synchronously delete the output script file(s)
 gulp.task( 'clean', del.bind( null, [ 'dist/assets/' ] ) );
+
+
+//Create a jquery bundled file
+gulp.task( 'jquery-bundle', [ 'clean' ], function () {
+  return gulp.src( config.jquerysrc )
+    .pipe( $.concat( 'jquery-bundle.js' ) )
+    .pipe( gulp.dest( 'lib' ) );
+} );
+
+//Create a bootstrap bundled file
+gulp.task( 'bootstrap-bundle', [ 'clean' ], function () {
+  return gulp.src( config.bootstrapsrc )
+    .pipe( $.concat( 'bootstrap-bundle.js' ) )
+    .pipe( gulp.dest( 'lib' ) );
+} );
+
+// Combine and the vendor files from bower into bundles (output to the Scripts folder)
+gulp.task( 'bundle-scripts', [ 'jquery-bundle', 'bootstrap-bundle' ], function () {
+  // runSequence( [ 'jquery-bundle', 'bootstrap-bundle' ] );
+} );
+
+//build lib scripts
+gulp.task( 'compile-lib', [ 'bundle-scripts' ], function () {
+  return gulp.src( 'lib/*.js' )
+    .pipe( $.sourcemaps.init() )
+    .pipe( $.concat( 'compiled-bundle.js' ) )
+    .pipe( gulp.dest( currentEnv.dist.js ) )
+    .pipe( $.rename( 'compiled-bundle.min.js' ) )
+    .pipe( $.uglify() )
+    .pipe( $.sourcemaps.write( './' ) )
+    .pipe( gulp.dest( currentEnv.dist.js ) );
+} );
 
 /**
  *
@@ -236,16 +280,16 @@ gulp.task( 'clean', del.bind( null, [ 'dist/assets/' ] ) );
  *
  */
 
-gulp.task( 'dist', [ 'html', 'sass', 'js', 'images', 'browser-sync' ], () => {
+gulp.task( 'dist', [ 'html', 'sass', 'js', 'images', 'fonts', 'compile-lib', 'browser-sync' ], () => {
 
-	// globbing
-	// matches any file with a .scss extension in dist/scss or a child directory
-	gulp.watch( currentEnv.src.sass + '**/*.scss', [ 'sass' ] );
-	gulp.watch( currentEnv.src.js + '*.js', [ 'js' ] );
-	gulp.watch( currentEnv.src.html + '*.html', [ 'html', 'bs-reload' ] );
-	gulp.watch( currentEnv.src.images + '**/**/*.{png,jpg,gif,svg,ico}', [ 'images' ] );
+  // globbing
+  // matches any file with a .scss extension in dist/scss or a child directory
+  gulp.watch( currentEnv.src.sass + '**/*.scss', [ 'sass' ] );
+  gulp.watch( currentEnv.src.js + '*.js', [ 'js' ] );
+  gulp.watch( currentEnv.src.html + '*.html', [ 'html', 'bs-reload' ] );
+  gulp.watch( currentEnv.src.images + '**/**/*.{png,jpg,gif,svg,ico}', [ 'images' ] );
 } );
 
 gulp.task( 'default', [ 'clean' ], () => {
-	gulp.start( 'dist' );
+  gulp.start( 'dist' );
 } );
