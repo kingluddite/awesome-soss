9ee761dcd729c0b22b98f47bbdc3e3f0d07ddbef
Philip Howley <howley.phil@gmail.com>
2016-09-13 18:13:24 -0700

    add custom jquery to toggle flex menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,11 @@
-(function ($, window, document, undefined) {
+( function ( $, window, document, undefined ) {
 
   'use strict';
 
-  $(function () {
-    // FastShell
-  });
+  $( function () {
+    $( '.toggle-nav' ).on( 'click', function () {
+      $( '.flex-nav ul' ).toggleClass( 'open' );
+    } );
+  } );
 
-})(jQuery, window, document);
+} )( jQuery, window, document );
