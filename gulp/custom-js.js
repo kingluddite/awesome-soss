6885494f60555fb0100fc05961d95231f8172e0f
Philip Howley <howley.phil@gmail.com>
2016-09-21 18:47:21 -0700

    separate config files

--- gulp/custom-js.js
+++ gulp/custom-js.js
@@ -1,29 +1,29 @@
-module.exports = function(gulp, plugins, paths, config, currentEnv, browserSync) {
-  return function() {
+module.exports = function ( gulp, plugins, paths, config, currentEnv, browserSync ) {
+  return function () {
     var $ = plugins;
-    var banner = require('../banner.js')();
-    var thePackage = require('../package.json');
+    var banner = require( './banner.js' )();
+    var thePackage = require( '../package.json' );
 
-    gulp.src(currentEnv.src.js + 'scripts.js')
-      .pipe($.sourcemaps.init())
-      .pipe($.jshint('.jshintrc'))
-      .pipe($.jshint.reporter('default'))
-      .pipe($.header(banner, {
+    gulp.src( currentEnv.src.js + 'scripts.js' )
+      .pipe( $.sourcemaps.init() )
+      .pipe( $.jshint( '.jshintrc' ) )
+      .pipe( $.jshint.reporter( 'default' ) )
+      .pipe( $.header( banner, {
         thePackage: thePackage
-      }))
-      .pipe(gulp.dest(currentEnv.dist.js))
-      .pipe($.uglify())
-      .pipe($.header(banner, {
+      } ) )
+      .pipe( gulp.dest( currentEnv.dist.js ) )
+      .pipe( $.uglify() )
+      .pipe( $.header( banner, {
         thePackage: thePackage
-      }))
-      .pipe($.rename({
+      } ) )
+      .pipe( $.rename( {
         suffix: '.min'
-      }))
-      .pipe($.sourcemaps.write())
-      .pipe(gulp.dest(currentEnv.dist.js))
-      .pipe(browserSync.reload({
+      } ) )
+      .pipe( $.sourcemaps.write() )
+      .pipe( gulp.dest( currentEnv.dist.js ) )
+      .pipe( browserSync.reload( {
         stream: true,
         once: true
-      }));
+      } ) );
   };
 };
